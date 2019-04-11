66a0d629ef7e3538207b2d69cb1b2a7d9707e071
Justin Coulter <justincoulter@icloud.com>
2019-04-11 08:42:27 -0400

    through 7f.

--- Homework_9.sql
+++ Homework_9.sql
@@ -74,7 +74,7 @@ ON staff.staff_id = payment.staff_id
 GROUP BY staff_id;
 #6c.
 USE sakila;
-SELECT film.film_id, film.title, SUM(film_actor.actor_id)
+SELECT film.film_id, film.title, COUNT(film_actor.actor_id)
 FROM film
 INNER JOIN film_actor
 ON film.film_id = film_actor.film_id
@@ -94,8 +94,69 @@ INNER JOIN customer
 ON payment.customer_id = customer.customer_id
 GROUP BY payment.customer_id
 ORDER BY last_name;
+#7a.
+USE sakila;
+SELECT title
+FROM film
+WHERE (title LIKE 'K%' or title LIKE 'Q%') and language_id = 1;
 
 
 
+#7b.
+SELECT first_name, last_name
+FROM actor 
+WHERE actor_id IN (
+	SELECT actor_id 
+	FROM film_actor
+	WHERE film_id =(
+		SELECT film_id 
+		FROM film 
+		WHERE title = 'Alone Trip'));
+        
+#7c.
+SELECT a.first_name, a.last_name, a.email
+FROM customer a
+JOIN address b
+ON a.address_id = b.address_id
+WHERE b.address_id IN (
+	SELECT b.address_id
+	FROM city a
+	JOIN address b
+	ON a.city_id = b.city_id
+	WHERE country_id = (SELECT country_id
+		FROM country
+		WHERE country = 'Canada'));
+        
+#7d.
+SELECT title 
+FROM film
+WHERE film_id IN (
+	SELECT film_id
+	FROM film_category
+	WHERE category_id IN (
+		SELECT category_id
+		FROM category
+		WHERE name = 'family'));
+        
+#7e.
+USE sakila;
+CREATE VIEW rental_count AS
+SELECT a.film_id, count(a.film_id) AS rental_count
+FROM inventory a
+JOIN rental b
+ON a.inventory_id = b.inventory_id
+GROUP BY film_id;
 
+SELECT a.title, b.film_id, b.rental_count
+FROM film a
+JOIN rental_count b
+ON a.film_id = b.film_id
+ORDER BY rental_count DESC;
+
+#7f.
+USE sakila;
+SELECT sales_by_store.store, CONCAT('$', sales_by_store.total_sales) AS total_sales
+FROM sales_by_store;
+
+#7h.
 
